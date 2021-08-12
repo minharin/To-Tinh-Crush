86048f49055a020c1b982b4cefb60a5fcceaf61e
minharin <88825216+minharin@users.noreply.github.com>
2021-08-12 16:21:00 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
     introTitle: 'Này đầu đất',
-    introDesc: `Em thấy nhớ anh, em
+    introDesc: `Em thấy nhớ anh,
     Em thấy rất nhớ anh thoi
     Ngày đêm cứ loay hoay
     Trong suy nghĩ về anh
     Anh có nhớ em không?
     Anh có thấy nhớ em không?`,
-    btnIntro: 'Hic hic 🥺🥺',
-    title: 'Anh mà thoát ra coi chừng khỏi cho chơi game đấy 😌😌',
-    desc: 'Anh có yêu em nhiều hongggg 🥺🥺',
-    btnYes: 'Yêu nhiều lắm luôn, muốn ăn luôn 🥺🥺',
-    btnNo: 'Hong, anh yêu bé hàng xóm 😗😗',
-    question:'Vì sao anh lại yêu em dợoooo 🥺🥺',
-    btnReply: 'Gửi cho em nà 😝😝',
-    reply: 'Vì em là em bé của mình anh thoiiii 😘😘',
-    mess: 'Em biết mà, hihi 😚😚',
-    messDesc: 'Vậy thì bớt chơi game lại đi:))',
-    btnAccept: 'Dạ, anh biết gòi. Yêu emmmmmmm 🙆🙆',
-    messLink: 'http://fb.com' //messages/t/100014015913631
+    btnIntro: 'Hic hic 🥺🥺🥺',
+    title: 'Anh mà thoát ra coi chừng hỏng máy đấy 😌😌😌',
+    desc: 'Anh có nhớ em nhiều hongggg 🥺🥺',
+    btnYes: 'Nhớ nhiều lắm luônnnnn 🥺🥺🥺',
+    btnNo: 'Hong, anh nhớ "girl next u" 😗😗😗',
+    question:'Vì sao anh lại nhớ em thíaaaaa 🥺🥺🥺',
+    btnReply: 'Gửi cho em nà 😝😝😝',
+    reply: 'Vì em chỉ nhớ mình anh thôiiii 😘😘😘',
+    mess: 'Em biết mà, hihi 😚😚😚',
+    messDesc: 'Vậy thì mình hẹn hò nhau thôi nhỉ:))',
+    btnAccept: 'Dạ. Yêu emmmmmmm 🙆🙆🙆',
+    messLink: 'http://fb.com' //messages/t/100022488302713
 }
